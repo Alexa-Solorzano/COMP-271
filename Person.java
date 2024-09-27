3fd7ad8373523301b42a203b97e49e3f45e8ffe3
Alexa-Solorzano <150977874+Alexa-Solorzano@users.noreply.github.com>
2024-09-27 02:01:27 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -66,11 +66,16 @@ public class Person implements Comparable<Person>, SillyActions {
 
     public void makeRandomSound(){
     /**
-    1) Declare an array with random sounds
-    2) using the random class, generate a random index number based on the length of the array
-    3) assign variable representing the random sound chosen and attach it to the random index 
-    4) print out sound
+    * 1) Declare an array with random sounds
+    * 2) using the random class, generate a random index number based on the length of the array
+    * 3) assign variable representing the random sound chosen and attach it to the random index 
+    * 4) print out sound
     */
+        String[] sounds = {"PLOP", "SPLAT", "GUSH", "HOOT", "HOWL", "POP", "CRUNCH", "BEEP", "SQUEAK", "DRIP", "BANG", "ROAR", "BLEAT", "SNAP", "FLUTTER", "CRACK", "PURR", "SIZZLE", "CLAP", "RATTLE"};
+        Random random = new Random();
+        int index = random.nextInt(sounds.length);
+        String randomSound = sounds[index];
+        System.out.println(randomSound);
     }
     public void performSillyDance(){
         System.out.println("The footwork for the Caballo Dorado - Payaso de Rodeo is complicated but simple.\nYou start off with one arm/fist in the air and the stomp with opposite foot to the beat." +
@@ -80,11 +85,11 @@ public class Person implements Comparable<Person>, SillyActions {
 
     public String reciteAlphabetBackwards(){
         /**
-        1) declare an array with all 26 letters of the alphabet 
-        2) declare a variable with a random number that will represent the element of the skipped letter
-        3) for loop that will print out the array backwards 
-        4) if(loop traversing is not the skipped element)
-           then print out the array  
+        * 1) declare an array with all 26 letters of the alphabet 
+        * 2) declare a variable with a random number that will represent the element of the skipped letter
+        * 3) for loop that will print out the array backwards 
+        * 4) if(loop traversing is not the skipped element)
+        *   then print out the array  
         */
         char[] alphabetOrder = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
         Random random = new Random();
@@ -101,11 +106,11 @@ public class Person implements Comparable<Person>, SillyActions {
     /** Count to ten in an unusual way (maybe skip a number) */
     public void countToTenWeirdly(){
     /**
-    1) declare an array with words in different languages counting to 10 
-    2) declare a variable with a random number that will represent the element of the skipped letter
-    3) for loop that will print out the array  
-    4) if(loop traversing is not the skipped element)
-           then print out the array  
+    * 1) declare an array with words in different languages counting to 10 
+    * 2) declare a variable with a random number that will represent the element of the skipped letter
+    * 3) for loop that will print out the array  
+    * 4) if(loop traversing is not the skipped element)
+    *      then print out the array  
     */
         String[] tenAroundTheWorld = {"UNO","DEUX", "THREE", "QUATRE", "FUNF", "SEIS", "SEPT", "ACHT", "NEUF", "TEN"};
         Random random = new Random();
