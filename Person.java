55a5d9d84f6d43e1cc239d53f25b966bb54a3d82
Alexa-Solorzano <150977874+Alexa-Solorzano@users.noreply.github.com>
2024-09-27 01:06:58 -0500

    Update Person.java

--- Person.java
+++ Person.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 public class Person implements Comparable<Person>, SillyActions {
 
     private static final String DEFAULT_LAST_NAME = "LNU";
@@ -63,4 +64,29 @@ public class Person implements Comparable<Person>, SillyActions {
         this.yearBorn = yearBorn;
     }
 
+    public void performSillyDance(){
+        System.out.println("The footwork for the Caballo Dorado - Payaso de Rodeo is complicated but simple.\nYou start off with one arm/fist in the air and the stomp with opposite foot to the beat." +
+                          "Once the music starts you take two steps to the right followed by two steps to the back. Then you take two steps to the back and two steps forward. From this position make a half turn" +
+                          "to your right and repeat the steps starting from the two steps to the right. Repeat until the 4 minuet and 14 second song is over.");
+    }
+
+    public String reciteAlphabetBackwards(){
+        /**
+        1) declare an array with all 26 letters of the alphabet 
+        2) declare a variable with a random number that will represent the element of the skipped letter
+        3) for loop that will print out the array backwards 
+        4) if(loop traverses this one element (the variable))
+           then print out the next element 
+        */
+        char[] alphabetOrder = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        Random random = new Random();
+        int skippedLetter = random.nextInt(26); //want a letter represent from element 0 to element 25
+        StringBuilder backwardsResult = new StringBuilder();
+        for(int i=25; i>=0; i--){
+            if(i != skippedLetter){
+                backwardsResult.append(alphabetOrder[i]).append(" ");
+            }
+        }
+        return backwardsResult.toString().trim();
+    }
 }
